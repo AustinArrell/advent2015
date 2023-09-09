f2796af5cc3a5ac1ee49a588bdf3c9892403c9b0
Austin Arrell <austinarrell@outlook.com>
2023-09-09 13:25:59 -0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace advent2015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
