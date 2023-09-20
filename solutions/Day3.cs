7f0a2a69473e18f06ea50f966dc6eb64aecc09f2
Austin Arrell <austinarrell@gmail.com>
2023-09-19 18:20:51 -0700

    add puzzle description for day 3

--- solutions/Day3.cs
+++ solutions/Day3.cs
@@ -1,9 +1,12 @@
 ﻿using BenchmarkDotNet.Attributes;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace advent2015.solutions
 {
@@ -20,14 +23,24 @@ namespace advent2015.solutions
             inputInstructions = File.ReadAllText($"D:\\dev\\dotNet\\advent2015\\input\\day3.txt");
         }
 
+        /*
+         * Santa is delivering presents to an infinite two-dimensional grid of houses.
+         * He begins by delivering a present to the house at his starting location, 
+         * and then an elf at the North Pole calls him via radio and tells him where 
+         * to move next.Moves are always exactly one house to the north (^), south(v), east(>), or west(<). 
+         * After each move, he delivers another present to the house at his new location.
+         * However, the elf back at the north pole has had a little too much eggnog, 
+         * and so his directions are a little off, and Santa ends up visiting some houses 
+         * more than once.How many houses receive at least one present?
+        */
+
         [Benchmark]
-        public int calculateNumberOfVisitedHouses() 
+        public int calculateNumberOfVisitedHouses()
         {
             (int, int) currentPosition = (0,0);
             HashSet<(int,int)> visitedPositions = new HashSet<(int,int)>();
             visitedPositions.Add(currentPosition);
-
-            foreach (var movementCommand in inputInstructions) 
+            foreach (var movementCommand in inputInstructions)
             {
                 switch (movementCommand) 
                 {
@@ -49,6 +62,15 @@ namespace advent2015.solutions
 
             return visitedPositions.Count();
         }
+
+        /*
+         * The next year, to speed up the process, Santa creates a robot version of himself, 
+         * Robo-Santa, to deliver presents with him. Santa and Robo-Santa start at the same
+         * location(delivering two presents to the same starting house), then take turns moving 
+         * based on instructions from the elf, who is eggnoggedly reading from the same script as the previous year.
+         * 
+         * This year, how many houses receive at least one present?
+        */
 
         [Benchmark]
         public int calculateNumberOfVisitedHousesRoboMode()
